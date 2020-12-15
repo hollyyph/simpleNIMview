6a715207d0ba9ff533367c0131815e312e5af148
hollyyph <hollyana.puteri.haryono@gmail.com>
2020-12-15 21:05:31 +0700

    change url

--- dev.env.js
+++ dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    VUE_APP_BACKEND_URL: '"http://localhost:5000"'
+    VUE_APP_BACKEND_URL: '"https://simplenim-app.herokuapp.com"'
 })
--- prod.env.js
+++ prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
     NODE_ENV: '"production"',
     ROOT_API: '"http://www.site.com/api"',
-    VUE_APP_BACKEND_URL: '"http://localhost:5000"'
+    VUE_APP_BACKEND_URL: '"hthttps://simplenim-app.herokuapp.com"'
 }
